62d8e6e132302ae4b68c9e8a005617abcd7c7044
Teddyz <lawyk-pa14@student.tarc.edu.my>
2019-01-08 18:26:35 +0800

    Updated homepage and post

--- app/src/main/java/com/example/lim/assignment/Feedback.java
+++ app/src/main/java/com/example/lim/assignment/Feedback.java
@@ -2,12 +2,30 @@ package com.example.lim.assignment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Feedback extends AppCompatActivity {
 
+    
+    private Spinner spinner;
+    private static final String[] paths = {"item 1", "item 2", "item 3"};
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+/*
+       spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Feedback.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+     */
+
     }
+
 }
