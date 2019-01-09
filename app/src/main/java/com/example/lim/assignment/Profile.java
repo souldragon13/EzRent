18439fd04290c91c0e8aa04e7b32c13d67e59a45
Teddyz <lawyk-pa14@student.tarc.edu.my>
2019-01-10 00:19:29 +0800

    update profile

--- app/src/main/java/com/example/lim/assignment/Profile.java
+++ app/src/main/java/com/example/lim/assignment/Profile.java
@@ -1,13 +1,25 @@
 package com.example.lim.assignment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Profile extends AppCompatActivity {
-
+    private ImageButton settingbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        settingbtn = (ImageButton)findViewById(R.id.setting_icon);
+
+        settingbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(Profile.this, Profile_Details.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/lim/assignment/SignUp.java
+++ app/src/main/java/com/example/lim/assignment/SignUp.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
